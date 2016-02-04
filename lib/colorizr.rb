9ce77c06fd11efb6a63c08353b90e298aab02f43
James Teitsworth <james.teitsworth@outlook.com>
2016-02-04 13:13:40 -0600

    Switch to define_method for dynamic method creation

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,27 +1,36 @@
 class String
-  # Set up parallel arrays for colors and format codes
-  @@colors = [:black, :red, :green, :yellow, :blue, :pink,
-              :light_blue, :light_grey, :white]
-  @@format_numbers = [30, 31, 32, 33, 34, 35, 36, 37, 97]
+  COLORS = [{ name: :black, code: 30 }, { name: :red, code: 31 },
+            { name: :green, code: 32 }, { name: :yellow, code: 33 },
+            { name: :blue, code: 34 }, { name: :pink, code: 35 },
+            { name: :light_blue, code: 36 }, { name: :light_grey, code: 37 },
+            { name: :white, code: 97 }].freeze
 
   def self.colors
-    @@colors
+    COLORS
   end
 
   def self.sample_colors
     # Using send method to call the dynamically generated functions
-    @@colors.each do |color|
-      puts "This is " + "#{color}".send(color)
+    COLORS.each do |color|
+      puts "This is " + "#{color[:name]}".send(color[:name])
     end
   end
 
+  # def self.create_colors
+  #   COLORS.each do |color|
+  #     self.class_eval %Q{
+  #       def #{color[:color]}
+  #         "\e[#{color[:code]}m" + self + "\e[0m"
+  #       end
+  #     }
+  #   end
+  # end
+
   def self.create_colors
-    @@colors.each_with_index do |color, index|
-      self.class_eval %Q{
-        def #{color}
-          "\e[#{@@format_numbers[index]}m" + self + "\e[0m"
-        end
-      }
+    COLORS.each do |color|
+      send(:define_method, color[:name]) do
+        "\e[#{color[:code]}m" + self + "\e[0m"
+      end
     end
   end
 end
